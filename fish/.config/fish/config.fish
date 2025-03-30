be60ef29454224481fd89f35ea5e27fd0eca4b74
Kevin Bonham, PhD <dev@bonham.ch>
2025-03-30 15:49:01 -0400

    add ssh config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,20 @@
 if status is-interactive
+    # Setup
+    set -Ux EDITOR nvim
     starship init fish | source
+    zoxide init fish | source
+    direnv hook fish | source
+
+    # General aliases
+    alias ls="eza"
+    alias cat="bat"
+    alias vi="nvim"
+
+    #git aliases
+    alias gc="git commit"
+    alias gl="git log --oneline --graph"
+    alias gcm="git checkout main"
+    alias gb="git branch"
+    alias gch="git checkout"
+
 end
