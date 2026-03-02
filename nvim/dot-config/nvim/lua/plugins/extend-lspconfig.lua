ef6aa2530d00ead6f569e1c94833f32a7ab9c856
Kevin Bonham <dev@bonham.ch>
2026-03-02 15:37:26 -0500

    Better tinymist support with pin

--- nvim/dot-config/nvim/lua/plugins/extend-lspconfig.lua
+++ nvim/dot-config/nvim/lua/plugins/extend-lspconfig.lua
@@ -1,36 +1,44 @@
 -- https://github.com/Myriad-Dreamin/tinymist/blob/main/editors/neovim/README.md#working-with-multiple-files-projects
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
+  {
+    "folke/which-key.nvim",
+    opts = {
+      spec = {
+        { "<leader>t", group = "Tinymist" },
+      },
+    },
   },
-  config = function()
-    require("mason").setup()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "tinymist" },
-    })
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tinymist = {},
+      },
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("LspAttach", {
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if not client or client.name ~= "tinymist" then return end
+          local bufnr = args.buf
 
-    local lspconfig = require("lspconfig")
+          vim.keymap.set("n", "<leader>tp", function()
+            client:exec_cmd({
+              title = "pin",
+              command = "tinymist.pinMain",
+              arguments = { vim.api.nvim_buf_get_name(0) },
+            }, { bufnr = bufnr })
+          end, { buffer = bufnr, desc = "[T]inymist [P]in" })
 
-    lspconfig.tinymist.setup({
-      on_attach = function(client, bufnr)
-        vim.keymap.set("n", "<leader>tp", function()
-          client:exec_cmd({
-            title = "pin",
-            command = "tinymist.pinMain",
-            arguments = { vim.api.nvim_buf_get_name(0) },
-          }, { bufnr = bufnr })
-        end, { desc = "[T]inymist [P]in", noremap = true })
-
-        vim.keymap.set("n", "<leader>tu", function()
-          client:exec_cmd({
-            title = "unpin",
-            command = "tinymist.pinMain",
-            arguments = { vim.v.null },
-          }, { bufnr = bufnr })
-        end, { desc = "[T]inymist [U]npin", noremap = true })
-      end,
-    })
-  end,
+          vim.keymap.set("n", "<leader>tu", function()
+            client:exec_cmd({
+              title = "unpin",
+              command = "tinymist.pinMain",
+              arguments = { vim.v.null },
+            }, { bufnr = bufnr })
+          end, { buffer = bufnr, desc = "[T]inymist [U]npin" })
+        end,
+      })
+    end,
+  },
 }
--- nvim/dot-config/nvim/lua/plugins/typst-preview.lua
+++ nvim/dot-config/nvim/lua/plugins/typst-preview.lua
@@ -3,4 +3,7 @@ return {
   ft = "typst",
   version = "1.*",
   opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  keys = {
+    { "<leader>tr", "<cmd>TypstPreview<cr>", desc = "[T]inymist P[r]eview" },
+  },
 }
