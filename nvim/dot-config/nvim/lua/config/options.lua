5142d6eadc18773239dbe112d70611d666d95d89
kevin <kevin@dhcp-130-64-46-214.boston.tufts.edu>
2026-04-02 14:51:17 -0400

    fix ssh copy

--- nvim/dot-config/nvim/lua/config/options.lua
+++ nvim/dot-config/nvim/lua/config/options.lua
@@ -27,3 +27,34 @@ vim.lsp.config("jetls", {
   filetypes = { "julia" },
 })
 vim.lsp.enable("jetls")
+
+-- Copy over ssh
+if vim.env.SSH_TTY then
+  local osc52 = require("vim.ui.clipboard.osc52")
+
+  local function copy_reg(reg)
+    local orig = osc52.copy(reg)
+    return function(lines, regtype)
+      -- Write to Vim's internal register
+      vim.fn.setreg(reg, table.concat(lines, "\n"), regtype)
+
+      -- Send OSC52 to local clipboard
+      orig(lines, regtype)
+    end
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52 with register sync",
+    copy = {
+      ["+"] = copy_reg("+"),
+      ["*"] = copy_reg("*"),
+    },
+    -- Do NOT use OSC52 paste, just use internal registers
+    paste = {
+      ["+"] = function() return vim.fn.getreg('+'), 'v' end,
+      ["*"] = function() return vim.fn.getreg('*'), 'v' end,
+    },
+  }
+
+  vim.o.clipboard = "unnamedplus"
+end
