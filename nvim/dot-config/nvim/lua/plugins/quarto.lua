95ecc76d5e284ce172b38770f5fbab614f68ce05
Kevin Bonham <dev@bonham.ch>
2026-04-02 09:22:33 -0400

    add some stuff for quarto and nvim

---
+++ nvim/dot-config/nvim/lua/plugins/quarto.lua
@@ -0,0 +1,15 @@
+-- plugins/quarto.lua
+return {
+  {
+    "quarto-dev/quarto-nvim",
+    dependencies = {
+      "jmbuhr/otter.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      codeRunner = {
+        default_method = "iron"
+      }
+    }
+  },
+}
