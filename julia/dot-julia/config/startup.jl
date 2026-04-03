b7cf54426d160acdbe4f3a7252b1c963ffd6dcdd
kevin <kevin@dhcp-130-64-46-214.boston.tufts.edu>
2026-04-03 08:18:50 -0400

    get terminal images working in tmux

--- julia/dot-julia/config/startup.jl
+++ julia/dot-julia/config/startup.jl
@@ -15,5 +15,39 @@ atreplinit() do repl
         using MiniLoggers: MiniLogger
         global_logger(MiniLogger())
     end
+
+    # In tmux, KittyTerminalImages.jl's raw APC sequences don't render correctly.
+    # Use kitten icat instead, which handles tmux passthrough properly.
+    if get(ENV, "TMUX", "") != "" && Sys.which("kitten") !== nothing
+        @eval begin
+            using ImageShow, ImageIO
+            struct KittyIcatDisplay <: AbstractDisplay end
+
+            function Base.display(::KittyIcatDisplay, x)
+                if showable(MIME("image/png"), x)
+                    mime, ext = MIME("image/png"), ".png"
+                elseif showable(MIME("image/svg+xml"), x)
+                    mime, ext = MIME("image/svg+xml"), ".svg"
+                else
+                    throw(MethodError(display, (KittyIcatDisplay(), x)))
+                end
+                buf = IOBuffer()
+                show(buf, mime, x)
+                tmpfile = tempname() * ext
+                try
+                    write(tmpfile, take!(buf))
+                    run(`kitten icat --align left $tmpfile`)
+                finally
+                    rm(tmpfile, force=true)
+                end
+            end
+
+            Base.displayable(::KittyIcatDisplay, ::MIME) = false
+            Base.displayable(::KittyIcatDisplay, ::MIME"image/png") = true
+            Base.displayable(::KittyIcatDisplay, ::MIME"image/svg+xml") = true
+
+            pushdisplay(KittyIcatDisplay())
+        end
+    end
 end
 
